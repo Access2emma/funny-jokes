51179e86b14c36ca1bad830a2d33026f9d301f30
Emmanuel Adeniyi <access2emma@gmail.com>
2019-02-10 00:15:43 +0100

    Initial commit

---
+++ src/Factory.php
@@ -0,0 +1,14 @@
+<?php
+
+class Factory
+{
+    protected $jokes = [
+        'First Joke of the year is not actually funny',
+        'This is the second joke'
+    ]; 
+    
+    public function __construct(array $jokes = null)
+    {
+        
+    }
+}
